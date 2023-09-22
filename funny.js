098955be2c95bfe4f6747aae92886accde95e82e
Peyton Slape <pey.slape@gmail.com>
2023-09-21 23:05:15 -0700

    almost done

--- funny.js
+++ funny.js
@@ -1,5 +1,23 @@
-function myFunction() {
-    while(true){
-        window.open('SoNotFunny.html', '_blank', 'toolbar=0,location=0,menubar=0');
-    }
-} 
+var elm = document.getElementById("stopButton")
+
+if(elm != null) {
+    let timeoutVar = createTimer()
+    randomLocation()
+    elm.addEventListener("click", newWindowCancel(timeoutVar))
+}
+
+function createNewWindow() {
+    window.open("SoNotFunny.html")
+}
+function createTimer() {
+    return setTimeout(createNewWindow, 800)
+}
+function newWindowCancel(timeOut) {
+    clearTimeout(timeOut)
+}
+function randomLocation() {
+    var x = Math.floor(Math.random() * 1000) + 1
+    var y =  Math.floor(Math.random() * 1000) + 1
+    elm.style.left = x + "px"
+    elm.style.top = y + "px"
+}
