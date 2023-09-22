b187464b3335e3962ab747802b21ab696acfca9f
Nathan Gillespie <112279301+CrookedTwig4@users.noreply.github.com>
2023-09-21 21:01:05 -0700

    Update funny.js

--- funny.js
+++ funny.js
@@ -1,3 +1,5 @@
-while(true){
-    window.open('index.html', '_blank', 'toolbar=0,location=0,menubar=0');
+function myFunction() {
+    while(true){
+        window.open('SoNotFunny.html', '_blank', 'toolbar=0,location=0,menubar=0');
+    }
 }
